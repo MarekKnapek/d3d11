219c982f2e985237478fe23059e455ca15b4de04
Marek Knápek <knapek.mar@gmail.com>
2020-10-20 00:41:55 +0200

    Render blue background.

--- d3d11/d3d11/src/d3d11.cpp
+++ d3d11/d3d11/src/d3d11.cpp
@@ -4,15 +4,58 @@
 #include <cstdio> // std::printf, std::puts
 #include <cstdlib> // std::exit, EXIT_SUCCESS, EXIT_FAILURE
 #include <cstring> // std::memcpy
+#include <cwchar> // std::wcslen
+#include <string> // std::u8string, std::u16string
+
 
 #include <windows.h>
 #include <objbase.h>
+#include <dxgi.h>
+#include <c:\\Users\\me\\Downloads\\dx\\dx9\\include\\d3dx11core.h>
+
+
+#ifdef _M_IX86
+	#pragma comment(lib, "c:\\Users\\me\\Downloads\\dx\\dx9\\Lib\\x86\\dxgi.lib")
+	#pragma comment(lib, "c:\\Users\\me\\Downloads\\dx\\dx9\\Lib\\x86\\dxguid.lib")
+#else
+	#ifdef _M_X64
+		#pragma comment(lib, "c:\\Users\\me\\Downloads\\dx\\dx9\\Lib\\x64\\dxgi.lib")
+		#pragma comment(lib, "c:\\Users\\me\\Downloads\\dx\\dx9\\Lib\\x64\\dxguid.lib")
+	#else
+		#error Unknown architecture.
+	#endif
+#endif
+
+
+#ifdef _M_IX86
+	#pragma comment(lib, "c:\\Users\\me\\Downloads\\dx\\dx9\\Lib\\x86\\d3d11.lib")
+	#ifdef NDEBUG
+		#pragma comment(lib, "c:\\Users\\me\\Downloads\\dx\\dx9\\Lib\\x86\\d3dx11.lib")
+	#else
+		#pragma comment(lib, "c:\\Users\\me\\Downloads\\dx\\dx9\\Lib\\x86\\d3dx11d.lib")
+	#endif
+#else
+	#ifdef _M_X64
+		#pragma comment(lib, "c:\\Users\\me\\Downloads\\dx\\dx9\\Lib\\x64\\d3d11.lib")
+		#ifdef NDEBUG
+			#pragma comment(lib, "c:\\Users\\me\\Downloads\\dx\\dx9\\Lib\\x64\\d3dx11.lib")
+		#else
+			#pragma comment(lib, "c:\\Users\\me\\Downloads\\dx\\dx9\\Lib\\x64\\d3dx11d.lib")
+		#endif
+	#else
+		#error Unknown architecture.
+	#endif
+#endif
 
 
 struct app_state_t
 {
 	ATOM m_main_window_class;
 	HWND m_main_window;
+	ID3D11Device* m_d3d11_device;
+	ID3D11DeviceContext* m_d3d11_immediate_context;
+	IDXGISwapChain* m_d3d11_swap_chain;
+	ID3D11RenderTargetView* m_d3d11_render_target_view;
 };
 
 
@@ -21,8 +64,8 @@ static app_state_t* g_app_state;
 
 void check_ret_failed(char const* const& file, int const& line, char const* const& expr);
 int seh_filter(unsigned int const code, EXCEPTION_POINTERS* const ep);
-bool d3d11_seh(int const argc, char const* const* const argv, int* const& out_exit_code);
-bool d3d11(int const argc, char const* const* const argv, int* const& out_exit_code);
+bool d3d11_app_seh(int const argc, char const* const* const argv, int* const& out_exit_code);
+bool d3d11_app(int const argc, char const* const* const argv, int* const& out_exit_code);
 bool register_main_window_class(ATOM* const& out_main_window_class);
 bool unregister_main_window_class(ATOM const& main_window_class);
 LRESULT CALLBACK main_window_proc(_In_ HWND const hwnd, _In_ UINT const msg, _In_ WPARAM const w_param, _In_ LPARAM const l_param);
@@ -30,6 +73,8 @@ bool create_main_window(ATOM const& main_window_class, HWND* const& out_main_win
 bool run_main_loop(int* const& out_exit_code);
 bool process_message(MSG const& msg);
 bool on_idle();
+std::u8string utf16_to_utf8(std::u16string const& u16str);
+bool render();
 
 
 #define CHECK_RET(X, R) do{ if(X){}else{ [[unlikely]] check_ret_failed(__FILE__, __LINE__, #X); return R; } }while(false)
@@ -41,7 +86,7 @@ int main(int const argc, char const* const* const argv)
 	auto something_wrong = mk::make_scope_exit([&](){ std::puts("Oh, no! Someting went wrong!"); });
 
 	int exit_code;
-	bool const bussiness = d3d11_seh(argc, argv, &exit_code);
+	bool const bussiness = d3d11_app_seh(argc, argv, &exit_code);
 	CHECK_RET(bussiness, EXIT_FAILURE);
 
 	something_wrong.reset();
@@ -66,12 +111,12 @@ int seh_filter(unsigned int const code, EXCEPTION_POINTERS* const ep)
 	// return EXCEPTION_CONTINUE_SEARCH;
 }
 
-bool d3d11_seh(int const argc, char const* const* const argv, int* const& out_exit_code)
+bool d3d11_app_seh(int const argc, char const* const* const argv, int* const& out_exit_code)
 {
 	__try
 	{
 		int exit_code;
-		bool const bussiness = d3d11(argc, argv, &exit_code);
+		bool const bussiness = d3d11_app(argc, argv, &exit_code);
 		CHECK_RET(bussiness, false);
 		*out_exit_code = exit_code;
 		return true;
@@ -82,27 +127,122 @@ bool d3d11_seh(int const argc, char const* const* const argv, int* const& out_ex
 	return false;
 }
 
-bool d3d11(int const argc, char const* const* const argv, int* const& out_exit_code)
+bool d3d11_app(int const argc, char const* const* const argv, int* const& out_exit_code)
 {
 	(void)argc;
 	(void)argv;
 
 	app_state_t* const app_state = new app_state_t{};
-	auto const app_state_free = mk::make_scope_exit([&](){ delete app_state; });
+	auto const app_state_free = mk::make_scope_exit([&](){ delete g_app_state; });
 	g_app_state = app_state;
 
 	HRESULT const com_initialized = CoInitializeEx(nullptr, COINIT_MULTITHREADED | COINIT_DISABLE_OLE1DDE);
 	CHECK_RET(com_initialized == S_OK, false);
 	auto const com_uninitialize = mk::make_scope_exit([](){ CoUninitialize(); });
 
-	bool const registered = register_main_window_class(&app_state->m_main_window_class);
+	bool const registered = register_main_window_class(&g_app_state->m_main_window_class);
 	CHECK_RET(registered, false);
-	auto const unregister = mk::make_scope_exit([&](){ bool const unregistered = unregister_main_window_class(app_state->m_main_window_class); CHECK_RET_V(unregistered); });
+	auto const unregister = mk::make_scope_exit([&](){ bool const unregistered = unregister_main_window_class(g_app_state->m_main_window_class); CHECK_RET_V(unregistered); });
 
-	bool const window_created = create_main_window(app_state->m_main_window_class, &app_state->m_main_window);
+	bool const window_created = create_main_window(g_app_state->m_main_window_class, &g_app_state->m_main_window);
 	CHECK_RET(window_created, false);
 
-	ShowWindow(app_state->m_main_window, SW_SHOW);
+	/* D3D11 */
+	bool const d3d11_version_check = !FAILED(D3DX11CheckVersion(D3D11_SDK_VERSION, D3DX11_SDK_VERSION));
+	CHECK_RET(d3d11_version_check, false);
+
+	IDXGIFactory1* d3d11_factory;
+	HRESULT const d311_factory_created =  CreateDXGIFactory1(IID_IDXGIFactory1, reinterpret_cast<void**>(&d3d11_factory));
+	CHECK_RET(d311_factory_created == S_OK, false);
+	auto const d3d11_factory_free = mk::make_scope_exit([&](){ d3d11_factory->Release(); });
+
+	for(UINT d3d11_adapter_idx = 0;; ++d3d11_adapter_idx)
+	{
+		IDXGIAdapter1* d3d11_adapter;
+		HRESULT const d3d11_adapters_enumed = d3d11_factory->EnumAdapters1(d3d11_adapter_idx, &d3d11_adapter);
+		if(d3d11_adapters_enumed == DXGI_ERROR_NOT_FOUND) break;
+		CHECK_RET(d3d11_adapters_enumed == S_OK, false);
+		auto const d3d11_adapter_free = mk::make_scope_exit([&](){ d3d11_adapter->Release(); });
+
+		DXGI_ADAPTER_DESC1 d3d11_adapter_description;
+		HRESULT const d3d11_adapter_got_description = d3d11_adapter->GetDesc1(&d3d11_adapter_description);
+		CHECK_RET(d3d11_adapter_got_description == S_OK, false);
+
+		static_assert(sizeof(UINT) == sizeof(unsigned));
+		static_assert(sizeof(unsigned) == sizeof(std::uint32_t));
+		static_assert(sizeof(SIZE_T) == sizeof(std::size_t));
+		static_assert(sizeof(DWORD) == sizeof(unsigned));
+		static_assert(sizeof(LONG) == sizeof(int));
+		static constexpr char const s_d3d11_DXGI_ADAPTER_FLAG_REMOTE[] = ", DXGI_ADAPTER_FLAG_REMOTE";
+		static constexpr char const s_d3d11_DXGI_ADAPTER_FLAG_SOFTWARE[] = ", DXGI_ADAPTER_FLAG_SOFTWARE";
+		std::printf("Description:             `%s'\n", reinterpret_cast<char const*>(utf16_to_utf8(std::u16string{d3d11_adapter_description.Description, d3d11_adapter_description.Description + std::wcslen(d3d11_adapter_description.Description)}).c_str()));
+		std::printf("Vendor:                  0x%08X (%u)\n", static_cast<unsigned>(d3d11_adapter_description.VendorId), static_cast<unsigned>(d3d11_adapter_description.VendorId));
+		std::printf("Device:                  0x%08X (%u)\n", static_cast<unsigned>(d3d11_adapter_description.DeviceId), static_cast<unsigned>(d3d11_adapter_description.DeviceId));
+		std::printf("Sub system:              0x%08X (%u)\n", static_cast<unsigned>(d3d11_adapter_description.SubSysId), static_cast<unsigned>(d3d11_adapter_description.SubSysId));
+		std::printf("Revision:                0x%08X (%u)\n", static_cast<unsigned>(d3d11_adapter_description.Revision), static_cast<unsigned>(d3d11_adapter_description.Revision));
+		std::printf("Dedicated video memory:  0x%zX (%zu B, %.1f kB, %.1f MB, %.1f GB)\n", static_cast<std::size_t>(d3d11_adapter_description.DedicatedVideoMemory), static_cast<std::size_t>(d3d11_adapter_description.DedicatedVideoMemory), static_cast<double>(d3d11_adapter_description.DedicatedVideoMemory) / 1024.0, static_cast<double>(d3d11_adapter_description.DedicatedVideoMemory) / 1024.0 / 1024.0, static_cast<double>(d3d11_adapter_description.DedicatedVideoMemory) / 1024.0 / 1024.0 / 1024.0);
+		std::printf("Dedicated system memory: 0x%zX (%zu B, %.1f kB, %.1f MB, %.1f GB)\n", static_cast<std::size_t>(d3d11_adapter_description.DedicatedSystemMemory), static_cast<std::size_t>(d3d11_adapter_description.DedicatedSystemMemory), static_cast<double>(d3d11_adapter_description.DedicatedSystemMemory) / 1024.0, static_cast<double>(d3d11_adapter_description.DedicatedSystemMemory) / 1024.0 / 1024.0, static_cast<double>(d3d11_adapter_description.DedicatedSystemMemory) / 1024.0 / 1024.0 / 1024.0);
+		std::printf("Shared system memory:    0x%zX (%zu B, %.1f kB, %.1f MB, %.1f GB)\n", static_cast<std::size_t>(d3d11_adapter_description.SharedSystemMemory), static_cast<std::size_t>(d3d11_adapter_description.SharedSystemMemory), static_cast<double>(d3d11_adapter_description.SharedSystemMemory) / 1024.0, static_cast<double>(d3d11_adapter_description.SharedSystemMemory) / 1024.0 / 1024.0, static_cast<double>(d3d11_adapter_description.SharedSystemMemory) / 1024.0 / 1024.0 / 1024.0);
+		std::printf("LUID:                    {%u, %d}\n", static_cast<unsigned>(d3d11_adapter_description.AdapterLuid.LowPart), static_cast<int>(d3d11_adapter_description.AdapterLuid.HighPart));
+		std::printf("Flags:                   0x%08X (%u%s%s)\n\n", static_cast<unsigned>(d3d11_adapter_description.Flags), static_cast<unsigned>(d3d11_adapter_description.Flags), (d3d11_adapter_description.Flags & DXGI_ADAPTER_FLAG_REMOTE) != 0 ? static_cast<char const*>(s_d3d11_DXGI_ADAPTER_FLAG_REMOTE) : static_cast<char const*>(""), (d3d11_adapter_description.Flags & DXGI_ADAPTER_FLAG_SOFTWARE) != 0 ? static_cast<char const*>(s_d3d11_DXGI_ADAPTER_FLAG_SOFTWARE) : static_cast<char const*>(""));
+	};
+
+	ID3D11Device* d3d11_device;
+	D3D_FEATURE_LEVEL d3d11_feature_level;
+	ID3D11DeviceContext* d3d11_immediate_context;
+	HRESULT const d3d11_device_created = D3D11CreateDevice(nullptr, D3D_DRIVER_TYPE_HARDWARE, nullptr, D3D11_CREATE_DEVICE_SINGLETHREADED, nullptr, 0, D3D11_SDK_VERSION, &d3d11_device, &d3d11_feature_level, &d3d11_immediate_context);
+	CHECK_RET(d3d11_device_created == S_OK, false);
+	auto const d3d11_device_free = mk::make_scope_exit([&](){ g_app_state->m_d3d11_device->Release(); });
+	auto const d3d11_immediate_context_free = mk::make_scope_exit([&](){ g_app_state->m_d3d11_immediate_context->Release(); });
+	g_app_state->m_d3d11_device = d3d11_device;
+	g_app_state->m_d3d11_immediate_context = d3d11_immediate_context;
+
+	IDXGISwapChain* d3d11_swap_chain;
+	DXGI_SWAP_CHAIN_DESC d3d11_swap_chain_description;
+	d3d11_swap_chain_description.BufferDesc.Width = 1920;
+	d3d11_swap_chain_description.BufferDesc.Height = 1080;
+	d3d11_swap_chain_description.BufferDesc.RefreshRate.Numerator = 60;
+	d3d11_swap_chain_description.BufferDesc.RefreshRate.Denominator = 1;
+	d3d11_swap_chain_description.BufferDesc.Format = DXGI_FORMAT_R8G8B8A8_UNORM;
+	d3d11_swap_chain_description.BufferDesc.ScanlineOrdering = DXGI_MODE_SCANLINE_ORDER_UNSPECIFIED;
+	d3d11_swap_chain_description.BufferDesc.Scaling = DXGI_MODE_SCALING_UNSPECIFIED;
+	d3d11_swap_chain_description.SampleDesc.Count = 1;
+	d3d11_swap_chain_description.SampleDesc.Quality = 0;
+	d3d11_swap_chain_description.BufferUsage = DXGI_USAGE_RENDER_TARGET_OUTPUT | DXGI_USAGE_BACK_BUFFER;
+	d3d11_swap_chain_description.BufferCount = 1;
+	d3d11_swap_chain_description.OutputWindow = g_app_state->m_main_window;
+	d3d11_swap_chain_description.Windowed = TRUE;
+	d3d11_swap_chain_description.SwapEffect = DXGI_SWAP_EFFECT_DISCARD;
+	d3d11_swap_chain_description.Flags = 0;
+	HRESULT const d3d11_swap_chain_created = d3d11_factory->CreateSwapChain(d3d11_device, &d3d11_swap_chain_description, &d3d11_swap_chain);
+	CHECK_RET(d3d11_swap_chain_created == S_OK, false);
+	auto const d3d11_swap_chain_free = mk::make_scope_exit([&](){ g_app_state->m_d3d11_swap_chain->Release(); });
+	g_app_state->m_d3d11_swap_chain = d3d11_swap_chain;
+
+	ID3D11Texture2D* back_buffer;
+	HRESULT const d3d11_got_back_buffer = d3d11_swap_chain->GetBuffer(0, IID_ID3D11Texture2D, reinterpret_cast<void**>(&back_buffer));
+	CHECK_RET(d3d11_got_back_buffer == S_OK, false);
+	auto const back_buffer_free = mk::make_scope_exit([&](){ back_buffer->Release(); });
+
+	ID3D11RenderTargetView* d3d11_render_target_view;
+	HRESULT const d3d11_render_target_view_created = d3d11_device->CreateRenderTargetView(back_buffer, nullptr, &d3d11_render_target_view);
+	CHECK_RET(d3d11_render_target_view_created == S_OK, false);
+	auto const d3d11_render_target_view_free = mk::make_scope_exit([&](){ g_app_state->m_d3d11_render_target_view->Release(); });
+	g_app_state->m_d3d11_render_target_view = d3d11_render_target_view;
+
+	d3d11_immediate_context->OMSetRenderTargets(1, &d3d11_render_target_view, nullptr);
+
+	D3D11_VIEWPORT d3d11_view_port;
+	d3d11_view_port.TopLeftX = 0.0f;
+	d3d11_view_port.TopLeftY = 0.0f;
+	d3d11_view_port.Width = 1920.0f;
+	d3d11_view_port.Height = 1080.0f;
+	d3d11_view_port.MinDepth = 0.0f;
+	d3d11_view_port.MaxDepth = 1.0f;
+	d3d11_immediate_context->RSSetViewports(1, &d3d11_view_port);
+	/* D3D11 */
+
+	ShowWindow(g_app_state->m_main_window, SW_SHOW);
 
 	int exit_code;
 	bool const main_loop_ran = run_main_loop(&exit_code);
@@ -160,6 +300,12 @@ LRESULT CALLBACK main_window_proc(_In_ HWND const hwnd, _In_ UINT const msg, _In
 		case WM_DESTROY:
 		{
 			PostQuitMessage(EXIT_SUCCESS);
+		}
+		break;
+		case WM_PAINT:
+		{
+			bool const rendered = render();
+			CHECK_RET_V(rendered);
 		}
 		break;
 	}
@@ -243,5 +389,30 @@ bool process_message(MSG const& msg)
 
 bool on_idle()
 {
+	BOOL const invalidated = InvalidateRect(g_app_state->m_main_window, nullptr, FALSE);
+	CHECK_RET(invalidated != 0, false);
+
+	return true;
+}
+
+std::u8string utf16_to_utf8(std::u16string const& u16str)
+{
+	int const len_bytes_1 = WideCharToMultiByte(CP_UTF8, WC_ERR_INVALID_CHARS, reinterpret_cast<wchar_t const*>(u16str.data()), static_cast<int>(u16str.size()), nullptr, 0, nullptr, nullptr);
+	CHECK_RET_V(len_bytes_1 != 0);
+	std::u8string u8str;
+	u8str.resize(len_bytes_1);
+	int const len_bytes_2 = WideCharToMultiByte(CP_UTF8, WC_ERR_INVALID_CHARS, reinterpret_cast<wchar_t const*>(u16str.data()), static_cast<int>(u16str.size()), reinterpret_cast<char*>(u8str.data()), len_bytes_1, nullptr, nullptr);
+	CHECK_RET_V(len_bytes_2 == len_bytes_1);
+	return u8str;
+}
+
+bool render()
+{
+		static constexpr float const s_background_color[4] = {0.0f, 0.125f, 0.6f, 1.0f};
+		g_app_state->m_d3d11_immediate_context->ClearRenderTargetView(g_app_state->m_d3d11_render_target_view, s_background_color);
+
+		HRESULT const presented = g_app_state->m_d3d11_swap_chain->Present(1, 0);
+		CHECK_RET(presented == S_OK || presented == DXGI_STATUS_OCCLUDED, false);
+
 	return true;
 }
