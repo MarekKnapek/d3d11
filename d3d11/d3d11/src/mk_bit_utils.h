be0ff99259d5b566ce16a93379949deb409ef608
Marek Knápek <knapek.mar@gmail.com>
2020-11-07 00:14:35 +0100

    typo

--- d3d11/d3d11/src/mk_bit_utils.h
+++ d3d11/d3d11/src/mk_bit_utils.h
@@ -8,7 +8,7 @@ namespace mk
 	constexpr bool is_power_of_two(unsigned const& n) noexcept
 	{
 		return n == 0 || (n & (n - 1)) == 0;
-	};
+	}
 
 	constexpr unsigned equal_or_next_power_of_two(unsigned const& n) noexcept
 	{
